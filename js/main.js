5788d160032807706de1f444cfb0ff83f283f51e
xita Rps <xitarps@gmail.com>
2020-11-15 01:35:27 -0300

    Add match logic

--- js/main.js
+++ js/main.js
@@ -9,7 +9,15 @@ const $ = selector => document.querySelector(selector);
 
 let gameCards = [];
 
+let messages = $('.messages');
 let domCards;
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+let matches = 0;
+let mismatches = 0;
+let score = 0;
 
 
 
@@ -99,19 +107,86 @@ function clearTable(){
 
 //get cards from DOM
 function getDomCards(){
-  domCards = document.querySelectorAll('.card')
+  domCards = [...document.querySelectorAll('.card')]
 }
 
-//add flip function to each card at the DOM
+//add flip function to each card at the DOM - using array domCards
 function addFlipToCards(){
   domCards.forEach(domCard => {
     domCard.addEventListener('click', flipCard);
   });
 }
+//remove flip function to each card at the DOM - using array domCards
+function removeFlipFromCards(){
+  domCards.forEach(domCard => {
+    domCard.removeEventListener('click', flipCard);
+  });
+}
+
+//remove single flip
+function removeSingleflipFrom(card){
+  card.removeEventListener('click', flipCard);
+}
+
+//reset user control -flip
+function resetFlipAction(){
+  removeFlipFromCards();
+  addFlipToCards();
+}
 
 //flip the card
 function flipCard(){
   this.classList.add('flip');
+  if(!hasFlippedCard){
+    hasFlippedCard = true;
+    firstCard = this;
+    removeSingleflipFrom(firstCard)
+    return;
+  }
+  secondCard = this;
+  removeFlipFromCards(); //prevent user to go on clicking other cards util check match
+  checkIfMatch(firstCard,secondCard)
+}
+
+//check if selected cards match
+function checkIfMatch(){
+  
+  let card1Name = firstCard.childNodes[0].src.split('/').pop()
+  card1Name = card1Name.split('.').shift()
+
+  let card2Name = secondCard.childNodes[0].src.split('/').pop()
+  card2Name = card2Name.split('.').shift()
+
+  if(card1Name == card2Name){
+    matches++;
+    updateScore();
+    domCards = domCards.filter((card)=>card.firstChild.src.split('/').pop().split('.').shift() != card1Name )// prevent multi click / bug
+    resetFlipAction();//allows user to keep selecting cards
+  }else{
+    mismatches++;
+    updateScore();
+    setTimeout(() => {
+      unFlipCards();
+      resetFlipAction();//allows user to keep selecting cards
+    }, 1000);
+  }
+
+  hasFlippedCard = false;
+  
+  if(domCards <=0)gameOver();
+}
+
+//update/view score
+function updateScore(){
+  messages.innerHTML = `Acertos: ${matches} - Erros: ${mismatches}`
+}
+
+//unflip cards
+function unFlipCards(){
+  firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip')
+  firstCard = null;
+  secondCard = null;
 }
 
 //build game
@@ -126,4 +201,9 @@ function startGame(){
 function clearGame(){
   clearTable();
   rebuildDecks();
+}
+
+//When reach the end of the game
+function gameOver(){
+  
 }
